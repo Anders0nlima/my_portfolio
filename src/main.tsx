7cd2102c66996f009b5be4457c27bd4d02293255
Anders0nlima <andersoncon85@gmail.com>
2026-03-11 09:12:17 -0300

    correcao de rota

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/my_portfolio">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       {/* PROVIDER DE LOADING (desativado) */}
       {/*
       <PageLoadingProvider>
