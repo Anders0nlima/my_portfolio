89c3b90989fb8683f8905f46dba371701596a584
Anders0nlima <andersoncon85@gmail.com>
2026-03-11 09:02:30 -0300

    config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Detecta se está publicando no GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my_portfolio/"
+  base: isGitHubPages ? '/my_portfolio/' : '/'
 })
