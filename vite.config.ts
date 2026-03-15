462100686502e4302cb38fff12549659e28533d4
Anders0nlima <andersoncon85@gmail.com>
2026-03-15 09:46:31 -0300

    conflito com githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // Detecta se está publicando no GitHub Pages
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+//const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: isGitHubPages ? '/my_portfolio/' : '/'
+  base: '/',
 })
